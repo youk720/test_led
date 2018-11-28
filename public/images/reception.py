50033f5713f86fe71c4054854cc093fa40d7e543
youk233_231 <tjm.yk0259@gmail.com>
2018-11-28 14:33:25 +0900

    2018_11_28_3

--- public/images/reception.py
+++ public/images/reception.py
@@ -3,48 +3,57 @@ import subprocess
 import RPi.GPIO as GPIO
 import time
 import os
+import sys
 
 # LEDピン設定
-led = 4
-
-# IPアドレス定義
-ip = 'http://10.3.100.116:8000/led'
+led = 17
+led_2 = 27
 
 #GPIO各種設定
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led, GPIO.OUT)
-GPIO.output(led, GPIO.LOW)
+GPIO.setup(led_2, GPIO.OUT)
 
-# While無限回し
+# LED初期発光防止用
+GPIO.output(led, GPIO.LOW)
+GPIO.output(led_2, GPIO.LOW)
+
+# IPアドレス定義
+ip = 'http://10.3.100.116:8000/led'
+# コマンド定義
+cmd = "curl -X GET %s" % (ip)
 
 try:
+    # While無限回し
     while True:
-        # prog = subprocess.Popen("curl -X GET %s" % (ip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        cmd = "curl -X GET %s" % (ip)
-        line = os.system(cmd)
-        line = str(line)
-        line = line.replace('\n', '')
-        line = line.replace('0', '')
+        # 上記で定義したコマンドをsubprocessで実行(コマンドラインで動かすのと同一のやり方)
+        prog = subprocess.run(cmd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+       # result =  str(sys.stdout.buffer.write(prog.stdout))
 
-        time.sleep(0.1)
-        # print("\n" + "hoge")
-        print('\n')
+        # 標準出力を文字列扱いでresultへ代入
+        result =  str(prog.stdout)
+        # resultのデバック用出力
+        # print('\n' + "result: " + result)
 
-        if line == ('led=True'):
+        # LED発光のためのif文
+        # if (A) in B:のような書き方で,Bの中にAが含まれているかを判定し,Trueの場合if文以下のコードを実行
+        if 'led=True' in result:
             GPIO.output(led, GPIO.HIGH)
-            print("\n" + "hoge_1")
-            # print(line)
-            # print('\n')
-            # GPIO.output(led_2, GPIO.LOW)
-        elif line == ('led=False'):
+
+            # if文入っているかのデバック用
+            # print("\n" + "hoge_1")
+        # LED発光のためのif文
+        elif 'led=False' in result:
             GPIO.output(led, GPIO.LOW)
-            print("\n" + "hoge_2")
-            # print(line)
-            # print('\n')
-            # GPIO.output(led_2, GPIO.HIGH)
 
+            # if文入っているかのデバック用
+            # print("\n" + "hoge_2")
 
+        # 特に書くこともないので通過
+        else:
+            pass
 
+# Curl + Cで終了させた時の処理
 except KeyboardInterrupt:
     # GPIOの終了処理
     GPIO.cleanup()
--- public/javascripts/view_index.js
+++ public/javascripts/view_index.js
@@ -1,35 +1,43 @@
 'use strict';
 // var $ = require("jquery");
 
+//  ONボタン操作時
 $('#on').on('click', function() {
   // something commands
   on();
 });
+// OFFボタン操作時
 $('#off').on('click', function() {
   // something commands
   off();
 });
-// module.exports = led_status;
+
+// Fキー操作時
 $('body').on("keydown", function(e) {
 if(e.keyCode === 70) {
   //70キー=Fキー off関数召喚
   on();
   }
 });
+// Dキー操作時
 $('body').on("keydown", function (m){
   if(m.keyCode === 68){
     //68 = dキー off関数召喚
   off();
   }
 });
+
 var socket = io();
+
+// 上でそれぞれ操作があった時に操作する関数
 function on(){
+  // socketで元々あった、値を消す
   socket.emit("sw status", '');
+  // 新しく発光するよう叩く
   socket.emit("sw status", "led=True");
-  $('#result').html("led=True");
 }
 function off(){
+  // 同上
   socket.emit("sw status", '');
   socket.emit("sw status", "led=False");
-  $('#result').html("led=False");
 }
