d9b97767de939132173197b186d48d62c728b33a
youk233_231 <tjm.yk0259@gmail.com>
2018-11-28 13:48:15 +0900

    2018_11_28_2

--- public/images/reception.py
+++ public/images/reception.py
@@ -13,6 +13,7 @@ ip = 'http://10.3.100.116:8000/led'
 #GPIO各種設定
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(led, GPIO.OUT)
+GPIO.output(led, GPIO.LOW)
 
 # While無限回し
 
@@ -20,18 +21,26 @@ try:
     while True:
         # prog = subprocess.Popen("curl -X GET %s" % (ip), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         cmd = "curl -X GET %s" % (ip)
-        line = str(os.system(cmd))
+        line = os.system(cmd)
+        line = str(line)
         line = line.replace('\n', '')
         line = line.replace('0', '')
+
         time.sleep(0.1)
+        # print("\n" + "hoge")
+        print('\n')
 
         if line == ('led=True'):
             GPIO.output(led, GPIO.HIGH)
-            print(line)
+            print("\n" + "hoge_1")
+            # print(line)
+            # print('\n')
             # GPIO.output(led_2, GPIO.LOW)
         elif line == ('led=False'):
             GPIO.output(led, GPIO.LOW)
-            print(line)
+            print("\n" + "hoge_2")
+            # print(line)
+            # print('\n')
             # GPIO.output(led_2, GPIO.HIGH)
 
 
